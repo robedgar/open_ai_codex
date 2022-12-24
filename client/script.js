ba13f42ce34bc30648b25cbfa5e4c2bd47220d61
robedgar <116982166+robedgar@users.noreply.github.com>
2022-12-24 13:21:28 +0000

    update v2

--- client/script.js
+++ client/script.js
@@ -10,8 +10,10 @@ function loader(element) {
   element.textContent = '';
 
   loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
     element.textContent += '.';
 
+    // If the loading indicator has reached three dots, reset it
     if (element.textContent === '....') {
       element.textContent = '';
     }
@@ -31,6 +33,9 @@ function typeText(element, text) {
   }, 20);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -41,17 +46,17 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-    <div class="wrapper ${isAi && 'ai'}">
-    <div class="chat">
-     <div className="profile">
-     <img
-     src="${isAi ? bot : user}"
-     alt="${isAi ? 'bot' : 'user'}"
-     />
-     </div>
-     <div class="message" id=${uniqueId}>${value}</div>
-    </div>
-    </div>
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
     `;
 }
 
@@ -60,24 +65,24 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
-  //user's chatstripe
-
+  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
+  // to clear the textarea input
   form.reset();
 
-  //bot's chatstripe
-
+  // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
 
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
-
-  //fetch from server -> bot's response
 
   const response = await fetch('https://codex-3qhq.onrender.com/', {
     method: 'POST',
@@ -90,18 +95,17 @@ const handleSubmit = async (e) => {
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
+  messageDiv.innerHTML = ' ';
 
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
 
     messageDiv.innerHTML = 'Something went wrong';
-
     alert(err);
   }
 };
